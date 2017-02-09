525f3698e8e577743f84430b3bcf509ac1c54ffa
Spencer Berenson <spencer@berenson.me>
2017-02-08 20:11:27 -0500

    Fixed student's page and scrolling.

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-let btnNavs = document.querySelectorAll(".btn-nav, .nav-link");
-btnNavs.forEach(function(element) {
-	element.addEventListener("click", function() {
-		// Hack used for anchor buttons to account for space of nav-bar.
-		setTimeout(function() { window.scrollBy(0, -64); }, 1);
-	});
-});
