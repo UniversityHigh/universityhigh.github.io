aa9a383799557bc5cd5d5b2dd387831ca2f57faa
Spencer Berenson <spencer@berenson.me>
2017-02-10 11:28:34 -0500

    Added functional calendar and basic custom CSS.

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,17 +1,4 @@
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let date = new Date();
-let month = months[date.getMonth()]
-let year = date.getFullYear();
-
-function daysInMonth() {
-	return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-}
-
-document.querySelector("#month").textContent = month + " ";
-document.querySelector("#year").textContent = year;
-
-let days = document.querySelectorAll(".days td");
-let dayNumbers = document.querySelectorAll(".days .day-number");
-for(let i = 0; i < daysInMonth(); i++) {
-	dayNumbers[i].textContent = i + 1;
-}
+$("#calendar").fullCalendar({
+	weekends: false
+});
+$(".fc-today-button").remove();
