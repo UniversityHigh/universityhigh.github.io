377c49d42660f753e98c48e8f47af082a9475795
Spencer Berenson <spencer@berenson.me>
2017-04-07 07:48:28 -0400

    Removed debug message. Oops.

--- assets/js/information.js
+++ assets/js/information.js
@@ -4,4 +4,3 @@ let monthNames = ["January", "February", "March", "April", "May", "June", "July"
 let month = date.getMonth() + 1;
 if(month.toString().length === 1) month = "0" + month.toString();
 document.querySelector("#pdf").src = "https://www.hartfordschools.org/wp-content/uploads/2016/01/Lunch-6-12-" + monthNames[month - 1] + year + "-Menu.pdf";
-alert(document.querySelector("#pdf").src);
