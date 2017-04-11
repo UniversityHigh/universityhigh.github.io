96efc90dc18d4567aa3c1b2b7a3473a2649ca423
Spencer Berenson <spencer@berenson.me>
2017-04-11 18:54:21 -0400

    Continuing restructure - restyling homepage.

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,11 @@ setInterval(function() {
 	(index + 1) >= banners.length ? index = 0 : index++; 
 	banner.css("background-image", "url(" + banners[index] + ")");
 }, 10000);
+
+$("#calendar").fullCalendar({
+	titleFormat: "[This Week at School]",
+	firstDay: 1,
+	defaultView: "basicWeek",
+	columnFormat: "ddd",
+	contentHeight: "auto"
+});
