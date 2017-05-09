97d9487cbac38cf30abe9bcff78e1bf2f5c4e231
Spencer Berenson <spencer@berenson.me>
2017-05-09 17:50:26 -0400

    Removed announcements temporarily until good design is available.

--- _harponica/assets/js/app.js
+++ _harponica/assets/js/app.js
@@ -27,34 +27,6 @@ $("li.dropdown :first-child").on("click", function() {
 	}
 });
 
-let announcements = $(".announcements p");
-if(announcements.length === 1) {
-	$("nav").append($(".announcements"));
-
-	announcements.on("click", function() {
-		alert(announcements.text());
-	});
-
-	let data = announcements.data("announcements");
-	let dataLength = data.length;
-	let dataIndex = 0;
-
-	announcements.text(data[dataIndex]);
-	announcements.animateCss("slideInRight", function() {
-		setInterval(function() {
-			dataIndex += 1;
-			if(dataIndex >= dataLength) {
-				dataIndex = 0;
-			}
-
-			announcements.animateCss("slideOutLeft", function() {
-				announcements.text(data[dataIndex]);
-				announcements.animateCss("slideInRight");
-			});
-		}, 10000);
-	});
-}
-
 $("#jump").on("click", function() {
 	$("html, body").animate({ scrollTop: 0 }, "slow");
 	return false;
