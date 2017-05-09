7588b1555520c8077ea1e3b89215ecaacae0a16e
Spencer Berenson <spencer@berenson.me>
2017-05-09 19:33:29 -0400

    Readded announcements for demo tomorrow.

--- _harponica/assets/js/index.js
+++ _harponica/assets/js/index.js
@@ -1,3 +1,31 @@
+let announcements = $(".announcements p");
+if (announcements.length === 1) {
+	$("nav").append($(".announcements"));
+
+	announcements.on("click", function () {
+		alert(announcements.text());
+	});
+
+	let data = announcements.data("announcements");
+	let dataLength = data.length;
+	let dataIndex = 0;
+
+	announcements.text(data[dataIndex]);
+	announcements.animateCss("slideInRight", function () {
+		setInterval(function () {
+			dataIndex += 1;
+			if (dataIndex >= dataLength) {
+				dataIndex = 0;
+			}
+
+			announcements.animateCss("slideOutLeft", function () {
+				announcements.text(data[dataIndex]);
+				announcements.animateCss("slideInRight");
+			});
+		}, 10000);
+	});
+}
+
 $("#gallery").slippry({
 	adaptiveHeight: false
 });
