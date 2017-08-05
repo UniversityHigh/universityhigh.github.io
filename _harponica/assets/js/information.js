11352027141bd8d56528b59424cfb7a6c94c8e66
Spencer Berenson <spencer@berenson.me>
2017-08-05 13:23:09 -0400

    :(

--- _harponica/assets/js/information.js
+++ _harponica/assets/js/information.js
@@ -1,12 +1,15 @@
-if(isMobile()) {
-	$(".aid > a, .update > a").each(function() {
-		let description = $(this).data("description");
-		$(this).attr("href", "javascript:alert(\"" + description + "\");");
-	});
-} else {
-	$(".aid > a, .update > a").each(function() {
-		let description = $(this).data("description");
-		$(this).attr("href", "javascript:void(0);").attr("title", description).attr("data-toggle", "tooltip").attr("data-trigger", "focus").attr("data-placement", "auto");
-	});
-	$("[data-toggle='tooltip']").tooltip();
-}
+$(".aid > a, .update > a").each(function() {
+	let description = $(this).data("description");
+	let descriptionLink = description.match(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig);
+
+	if(isMobile()) {
+		descriptionLink ? $(this).attr("href", descriptionLink[0]).attr("target", "_blank") : $(this).attr("href", "javascript:alert(\"" + description + "\");");
+	} else {
+		if(descriptionLink) {
+			$(this).attr("href", descriptionLink[0]).attr("target", "_blank");
+		} else {
+			$(this).attr("href", "javascript:void(0);").attr("title", description).attr("data-toggle", "tooltip").attr("data-trigger", "focus").attr("data-placement", "auto");
+			$("[data-toggle='tooltip']").tooltip();
+		}
+	}
+});
